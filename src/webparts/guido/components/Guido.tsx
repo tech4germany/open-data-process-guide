852512b20ac71ce5f65ac2419a619f524e8e9d52
Benjamin Degenhart <b.degenhart.work@gmail.com>
2020-09-21 17:01:14 +0200

    Repurpose as dev button, test nanoid and parsing URL params

--- src/webparts/guido/components/Guido.tsx
+++ src/webparts/guido/components/Guido.tsx
@@ -3,8 +3,17 @@ import styles from './Guido.module.scss';
 import { IGuidoWebPartProps } from '../GuidoWebPart';
 import { escape } from '@microsoft/sp-lodash-subset';
 import * as Fabric from 'office-ui-fabric-react';
+import { nanoid } from 'nanoid';
+import { parse } from 'query-string';
 
 export default function GuidoWebPart(props: IGuidoWebPartProps) {
+
+  const dev = () => {
+    console.log("nanoid: ", nanoid(5));
+    let parsed = parse(location.search);
+    console.log("URL params: ", parsed);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.row}>
@@ -12,7 +21,7 @@ export default function GuidoWebPart(props: IGuidoWebPartProps) {
           <span className={styles.title}>Welcome to SharePoint!</span>
           <p>Customize SharePoint experiences using Web Parts.</p>
           <p>{escape(props.description)}</p>
-          <Fabric.PrimaryButton href="https://aka.ms/spfx">Learn more</Fabric.PrimaryButton>
+          <Fabric.PrimaryButton onClick={dev}>Dev</Fabric.PrimaryButton>
         </div>
       </div>
     </div>
