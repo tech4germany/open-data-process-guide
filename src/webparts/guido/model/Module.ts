854203bfd9812716e78174360a0e2e7c81abe306
Benjamin Degenhart <b.degenhart.work@gmail.com>
2020-09-23 15:15:04 +0200

    Uncompress ref-fields in Module config

--- src/webparts/guido/model/Module.ts
+++ src/webparts/guido/model/Module.ts
@@ -1,9 +1,24 @@
 import * as modulesConfig from './modules.json';
 
 export class Module {
+    public config: any;
 
     constructor(public id: string) {
-        console.log(modulesConfig.modules)
+        this.config = modulesConfig.modules[id];
+
+        let fieldsUncompressed = {};
+        let fieldKeys = Object.keys(this.config.fields);
+        for (let i = 0; i < fieldKeys.length; i++) {
+            let key = fieldKeys[i];
+            let val = this.config.fields[key];
+            if (key === 'ref') {
+                fieldsUncompressed[val] = modulesConfig.fields[val];
+            } else {
+                fieldsUncompressed[key] = val;
+            }
+        }
+
+        this.config.fields = fieldsUncompressed
     }
 
 }
