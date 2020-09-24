38d76a7340973697b7927eaf86bff2ae16d735c8
Benjamin Degenhart <b.degenhart.work@gmail.com>
2020-09-24 14:13:50 +0200

    A case consists of tasks

--- src/webparts/guido/view/Case.tsx
+++ src/webparts/guido/view/Case.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { useEffect, useState } from "react";
 import {Process} from "../model/Process";
+import Task from "./Task";
 
 export interface ICaseProps {
     process: Process;
@@ -9,15 +10,23 @@ export interface ICaseProps {
 export default function Case(props: ICaseProps) {
 
     const [process, setProcess] = useState(null);
+    const [step, setStep] = useState(0);
+    const [tasks, setTasks] = useState([]);
 
     useEffect(() => {
         if (!process) {
             setProcess(props.process);
+            setTasks(props.process.modules.map(module => <Task module={module}/>))
         }
     });
 
     return (
-        <>
-        </>
+        tasks.length > 0 && (
+            <>
+                <i>Step: {step + 1}/{tasks.length}</i>
+                <br/><br/>
+                {tasks[step]}
+            </>
+        )
     );
 }
---
+++ src/webparts/guido/view/Task.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { useEffect, useState } from "react";
+import {Module} from "../model/Module";
+
+export interface ITaskProps {
+    module: Module;
+}
+
+export default function Task(props: ITaskProps) {
+
+    const [module, setModule] = useState(null);
+
+    useEffect(() => {
+        if (!module) {
+            setModule(props.module);
+        }
+    });
+
+    return (
+       module &&  (
+            <>
+                <b>{module.config.name}</b>
+                <br/>
+                {module.config.description}
+            </>
+        )
+    );
+}
