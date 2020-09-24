ecf00d42391dd1a1ed5947d20b16e0f14b75ddfa
Benjamin Degenhart <b.degenhart.work@gmail.com>
2020-09-24 13:38:34 +0200

    Add abstract model description

--- src/webparts/guido/model/Model.ts
+++ src/webparts/guido/model/Model.ts
@@ -3,6 +3,13 @@ import { Process } from "./Process";
 
 export class Model {
 
+    /*
+     One "process" consists of multiple "modules" that have "fields".
+     Modules are connected in an ordered succession with parallel as well as serial sections. TODO
+     Fields might have dependencies on other fields (in previous modules) and can be automatically filled. TODO
+     An instantiated process we call "case" containing instantiated modules we call "tasks".
+     */
+
     public processes: Process[] = [];
 
     constructor() {
