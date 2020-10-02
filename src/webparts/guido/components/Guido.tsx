d389db25c241add3410f900842ace59d36e70065
Benjamin Degenhart <b.degenhart.work@gmail.com>
2020-10-02 10:54:07 +0200

    Add wip code snippets to add fields and items to lists

--- src/webparts/guido/components/Guido.tsx
+++ src/webparts/guido/components/Guido.tsx
@@ -13,6 +13,9 @@ import { sp } from "@pnp/sp";
 import "@pnp/sp/webs";
 import "@pnp/sp/lists";
 import "@pnp/sp/items";
+import { IItemAddResult } from "@pnp/sp/items";
+import "@pnp/sp/fields/web";
+import "@pnp/sp/fields/list";
 
 export default function GuidoWebPart(props: IGuidoWebPartProps) {
 
@@ -39,6 +42,16 @@ export default function GuidoWebPart(props: IGuidoWebPartProps) {
         const list = sp.web.lists.getByTitle("listy");
         const allItems: any[] = await list.items.getAll();
         console.log(allItems);
+        /*list.fields.addText("NewField").then(f => {
+            console.log(f);
+        });
+        const newItem: IItemAddResult = await list.items.add({
+            Title: "foo",
+            NewField: "bar"
+        });
+        console.log(newItem);
+        const listEnsureResult = await sp.web.lists.ensure("created-list");
+        */
     }
 
     const dev = () => {
