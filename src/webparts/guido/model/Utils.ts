05336f6a3805d5334dc84c84f2116da7cde60a4b
Benjamin Degenhart <b.degenhart.work@gmail.com>
2020-10-03 22:48:55 +0200

    Use moment package to display timestamps nicely

--- src/webparts/guido/model/Utils.ts
+++ src/webparts/guido/model/Utils.ts
@@ -1,9 +1,14 @@
 import { Environment, EnvironmentType } from "@microsoft/sp-core-library";
+import * as moment from "moment";
 
 export default class Utils {
 
     public static isDevEnv(): boolean {
         return Environment.type === EnvironmentType.Local;
         // EnvironmentType.SharePoint, EnvironmentType.ClassicSharePoint
+    }
+
+    public static getFormattedTime(timestamp: number): any {
+        return moment.unix(timestamp).calendar();
     }
 }
