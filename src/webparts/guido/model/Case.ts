3b3ddc7f4d5f052c8a0590914fa9449c1fe665e3
Benjamin Degenhart <b.degenhart.work@gmail.com>
2020-10-04 11:44:02 +0200

    Prepare for Case to track its values

--- src/webparts/guido/model/Case.ts
+++ src/webparts/guido/model/Case.ts
@@ -7,9 +7,18 @@ export class Case {
     public startTime: number;
     public process: Process;
     public listID: number = null;
+    public step: number = 0;
+    public values: any = {};
 
     public initNewCase(process: Process) {
         this.process = process;
+        // copy all fields from the modules over here to track their values here
+        process.modules.map(module => {
+            this.values[module.id] = {};
+            Object.keys(module.config.fields).map(fieldId => {
+                this.values[module.id][fieldId] = null;
+            });
+        });
         this.id = process.id + '_' + nanoid(4);
         this.startTime = Date.now() / 1000;
     }
@@ -18,13 +27,17 @@ export class Case {
         this.id = caseConf.id;
         this.startTime = caseConf.startTime;
         this.process = process;
+        this.step = caseConf.step;
+        this.values = caseConf.values;
     }
 
     public getJSONconfig(): any {
         return {
             id: this.id,
             processId: this.process.id,
-            startTime: this.startTime
+            startTime: this.startTime,
+            step: this.step,
+            values: this.values
         }
     }
 
