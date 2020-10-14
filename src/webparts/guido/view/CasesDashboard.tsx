c5a58873293af1d8c45bbecd20e11ff285e8c4f2
Benjamin Degenhart <b.degenhart.work@gmail.com>
2020-10-14 22:36:25 +0200

    Fix typo
    Co-Authored-By: dangerousdani <dangerousdani@users.noreply.github.com>

--- src/webparts/guido/view/CasesDashboard.tsx
+++ src/webparts/guido/view/CasesDashboard.tsx
@@ -80,7 +80,7 @@ export default function CasesDashboard(props: ICaseDashboardProps) {
         <>
             <span className={styles.title}>Willkommen, {props.model && props.model.getCurrentUser().displayName}!</span>
             <br/><br/>
-            Danke das Sie Open Data für Ihr Ministerium bereit stellen möchten.
+            Danke, dass Sie Open Data für Ihr Ministerium bereit stellen möchten.
             <br/><br/>
             Guido hilft Ihnen dabei Ihre Datensätze durch den Prozess der Datenbereitstellung zu führen.
             <br/>
